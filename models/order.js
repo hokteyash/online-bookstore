07acccf622deb0c9092af806dbeb4abe9d71eb03
Yash Hokte <hokteyash4@gmail.com>
2025-07-16 00:41:03 +0530

    Added Order and orderItem model, it's schema

---
+++ models/order.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    user_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    shipping_address : {
+      street_name: {
+        type: String,
+      },
+      house_no: {
+        type: Number,
+      },
+      pincode: {
+        type: Number,
+        required: true,
+      },
+      city: {
+        type: String,
+      },
+      state: {
+        type: String,
+      },
+      country: {
+        type: String,
+      },
+    },
+    total_price:{
+        type:Number,
+        required:true
+    },
+    payment_method:{
+        type:String,
+        enum:['cod','upi'],
+        default:'upi'
+    },
+    payment_status:{
+        type:String,
+        enum:['pending','paid','failed','refunded'],
+        default:'pending'
+    },
+    order_status:{
+        type:String,
+        enum:['pending','shipped','delivered','cancelled'],
+        default:'pending'
+    },
+    paidAt:{
+        type:Date
+    },
+    shippedAt:{
+        type:Date
+    },
+    deliveredAt:{
+        type:Date
+    }
+},{
+    timestamps:true,
+})
+
+module.exports = mongoose.model('Order', orderSchema);
