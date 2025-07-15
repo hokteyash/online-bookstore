9ea0c2c9469629fbff04abc46c7e8c966f7d1629
Yash Hokte <hokteyash4@gmail.com>
2025-07-16 01:17:16 +0530

    Added some comments for help

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -1,34 +1,26 @@
-const Order = require('../models/order');
+const Order = require("../models/order");
+const OrderItem = require("../models/orderItem");
 
-const allOrders = async (req, res) => {
+const allOrders = async (req, res) => {};
 
-}
+const orderDetails = async (req, res) => {};
 
-const orderDetails = async (req, res) => {
-    
-}
+const createOrder = async (req, res) => {};
 
-const createOrder = async (req, res) => {
-    
-}
+const adminViewAllOrders = async (req, res) => {};
 
-const adminViewAllOrders = async (req,res) => {
-
-}
-
-const updateOrderStatus = async (req, res) => {
-
-}
+const updateOrderStatus = async (req, res) => {};
 
 const paymentHappening = async (req, res) => {
-
-}
+    // after payment completed, when we are updating in Order payment paid,
+    // so post will store each book of that order in "orderItem" table
+};
 
 module.exports = {
-    allOrders,
-    orderDetails,
-    createOrder,
-    adminViewAllOrders,
-    updateOrderStatus,
-    paymentHappening
-}
+  allOrders,
+  orderDetails,
+  createOrder,
+  adminViewAllOrders,
+  updateOrderStatus,
+  paymentHappening,
+};
